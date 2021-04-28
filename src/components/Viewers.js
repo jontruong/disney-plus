07019ef7e15fca0cfb172b3e633c31e7a1b3ddfd
Jonathan Truong <jjtruong90@gmail.com>
2021-04-28 15:27:31 -0700

    Adds videos

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -5,32 +5,32 @@ const Viewers = () => {
     return (
         <Container>
            <Wrap>
-               <img src="/images/viewers-disney.png" alt=""></img>
-               <video autoPlay={true} loop={true} playsInLine={true}>
+               <img src="/images/viewers-disney.png" alt=""/>
+               <video autoPlay muted loop playsInLine>
                    <source src ="/videos/1564674844-disney.mp4" type="video/mp4"/>
                </video>
            </Wrap>
            <Wrap>
-               <img src="/images/viewers-pixar.png" alt=""></img>
-               <video autoPlay={true} loop={true} playsInLine={true}>
+               <img src="/images/viewers-pixar.png" alt=""/>
+               <video autoPlay={true} muted loop playsInLine>
                    <source src ="/videos/1564676714-pixar.mp4" type="video/mp4"/>
                </video>
            </Wrap>
            <Wrap>
-               <img src="/images/viewers-marvel.png" alt=""></img>
-               <video autoPlay={true} loop={true} playsInLine={true}>
+               <img src="/images/viewers-marvel.png" alt=""/>
+               <video autoPlay muted loop playsInLine>
                    <source src ="/videos/1564676115-marvel.mp4" type="video/mp4"/>
                </video>
            </Wrap>
            <Wrap>
-               <img src="/images/viewers-national.png" alt=""></img>
-               <video autoPlay={true} loop={true} playsInLine={true}>
+               <img src="/images/viewers-national.png" alt=""/>
+               <video autoPlay muted loop playsInLine>
                    <source src ="/videos/1564676296-national-geographic.mp4" type="video/mp4"/>
                </video>
            </Wrap>
            <Wrap>
-               <img src="/images/viewers-starwars.png" alt=""></img>
-               <video autoPlay={true} loop={true} playsInLine={true}>
+               <img src="/images/viewers-starwars.png" alt=""/>
+               <video autoPlay muted loop playsInLine>
                    <source src ="/videos/1608229455-star-wars.mp4" type="video/mp4"/>
                </video> 
            </Wrap>
@@ -73,6 +73,25 @@ img {
     width: 100%;
     z-index: 1;
     top: 0;
+}
+
+video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    opacity: 0;
+    z-index: 0;
+}
+
+&:hover {
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb( 0 0 0 / 73%) 0px 16px 10px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+    
+    video {
+        opacity: 1;
+    }
 }
 
 `
